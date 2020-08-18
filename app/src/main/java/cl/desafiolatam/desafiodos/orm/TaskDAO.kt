3d7c7cd1e6ce8ee9c0151b0dc61363eeb08cde24
Alexis Rebolledo <git@puntogestion.cl>
2020-08-18 17:33:51 -0400

    subida

---
+++ app/src/main/java/cl/desafiolatam/desafiodos/orm/TaskDAO.kt
@@ -0,0 +1,18 @@
+package cl.desafiolatam.desafiodos.orm
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface TaskDAO {
+
+    @Query("SELECT * FROM Task")
+    fun getAll(): List<Task>
+
+    @Insert
+    fun insertTask(tarea: Task)
+
+    //@Delete
+}
